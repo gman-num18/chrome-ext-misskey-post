e734152f25da36adb3b873bfa273ff1bdec839b8
guitarman-num18 <nymwtg@icloud.com>
2026-02-15 18:27:21 +0900

    1st

---
+++ background.js
@@ -0,0 +1,35 @@
+// アドオンのアクションボタンが押された時、タブを引数として実行
+chrome.action.onClicked.addListener( async (tab)=>{
+    // デフォルト値
+    let misskey_domain = 'misskey.io';
+    // オプション画面で設定したドメイン名をストレージから取得
+    let rtnPromise = await chrome.storage.sync.get(
+        { misskey_domain: 'misskey.io' }, (result)=>{
+            misskey_domain = result.misskey_domain;
+            chrome.scripting.executeScript({
+                target: {tabId: tab.id},
+                function: injectedFunc,
+                args: [misskey_domain]
+            });
+        });
+});
+
+const injectedFunc = (domain)=>{
+    const s = screen;
+    const e = encodeURIComponent;
+    let sel = '';
+    if( document.getSelection().toString().length != 0){
+        sel = `> ${document.getSelection().toString()}`;
+    }
+    window.open(
+        "https://"+ domain +"/share?text="+
+        e(sel)  + "%0A%0A" +
+        e(document.title.replace(
+            new RegExp('https://t.co/[A-Za-z]+','g'),''
+        ))+" "+e(location.href),
+        "_blank",
+        "width=550,height=420,left="+
+        (s.availLeft+s.availWidth/2-275)+
+        ",top="+(s.availTop+s.availHeight/2-210)
+    );
+}
